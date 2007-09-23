284803d4913474484ee78eb296028cb84fbed4e0
aleiby <aleiby@1c315120-843a-0410-b846-4b95d8cac55e>
2007-09-23 04:16:25 +0000

    Dragging support. Text updating. Alpha'd background.

--- trunk/App.m
+++ trunk/App.m
@@ -4,6 +4,71 @@
 
 #import "App.h"
 
+#import <UIKit/UIView-Rendering.h>
+#import <LayerKit/LKLayer.h>
+
+#ifndef GRAPHICSSERVICES_H
+#define GRAPHICSSERVICES_H
+
+#import <UIKit/UIKit.h>
+
+struct __GSEvent;
+typedef struct __GSEvent GSEvent;
+typedef GSEvent *GSEventRef;
+
+struct CGPoint;
+struct CGRect;
+typedef struct CGPoint CGPoint;
+typedef struct CGRect CGRect;
+
+int GSEventIsChordingHandEvent(GSEvent *ev);
+int GSEventGetClickCount(GSEvent *ev);
+CGRect GSEventGetLocationInWindow(GSEvent *ev);
+float GSEventGetDeltaX(GSEvent *ev);
+float GSEventGetDeltaY(GSEvent *ev);
+CGRect  GSEventGetInnerMostPathPosition(GSEvent *ev);
+CGRect GSEventGetOuterMostPathPosition(GSEvent *ev);
+
+void GSEventVibrateForDuration(float secs);
+
+#endif
+
+@interface DragView : UIView
+{
+	UIView* _element;
+}
+- (void)setElement:(UIView*)element;
+- (void)updatePos:(GSEvent*)event;
+@end
+
+@implementation DragView
+
+- (void)setElement:(UIView*)element
+{
+	_element = element;
+}
+
+- (void)updatePos:(GSEvent*)event
+{
+    CGRect location = GSEventGetLocationInWindow(event);
+	[(UITextLabel*)_element setText:[NSString stringWithFormat:@"drag: %f %f", location.origin.x, location.origin.y]];
+	[_element setOrigin:location.origin];
+	[_element setNeedsDisplay];
+}
+
+- (void)mouseDragged:(GSEvent*)event
+{
+	[self updatePos:event];
+}
+
+- (void)mouseDown:(GSEvent*)event
+{
+	[self updatePos:event];
+}
+
+@end
+
+
 @implementation iPhonePuzzleLeague
 
 - (void) applicationDidFinishLaunching: (id) unused
@@ -25,7 +90,7 @@
 	mainViewRect = windowRect;
 	mainViewRect.origin.x = 0.0f;
 	mainViewRect.origin.y = 0.0f;
-	mainView = [[UIView alloc] initWithFrame: mainViewRect];
+	mainView = [[DragView alloc] initWithFrame: mainViewRect];
     [window setContentView: mainView];
 
 	//Set up the labels.
@@ -34,6 +99,11 @@
 	[mainView addSubview: testLabel];
 
 	[testLabel setText: @"iPhonePuzzleLeague"];
+
+	float background[4] = {1,0,0,0.5};
+	[testLabel setBackgroundColor: CGColorCreate(CGColorSpaceCreateDeviceRGB(), background)];
+
+	[(DragView*)mainView setElement: testLabel];
 }
 
 @end
